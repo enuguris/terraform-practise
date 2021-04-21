6c64d76905a10c947cf84f7e7d3e56c0d14bde91
enuguris <sukumarguptahyd@gmail.com>
2021-04-21 14:22:35 +0530

    few moer changes

--- network/sg.tf
+++ network/sg.tf
@@ -1,3 +1,10 @@
+locals {
+  tags = {
+    maintained_by = "terraform"
+    source_region = "us-east-2"
+  }
+}
+
 module "general-securitygroup" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "3.18.0"
@@ -10,6 +17,30 @@ module "general-securitygroup" {
   ingress_cidr_blocks = ["10.0.0.0/16", "0.0.0.0/0"]
   ingress_rules       = ["ssh-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
+
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "nfs-tcp"
+      cidr_blocks = "10.100.1.10/32"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "30.30.30.30/32"
+    },
+    {
+      from_port   = 10
+      to_port     = 20
+      protocol    = 6
+      description = "Service name"
+      cidr_blocks = "10.10.0.0/20"
+    },
+  ]
+
+  tags = merge({
+    Name = "general-securitygroup"
+    Env  = "Prod"
+  }, local.tags)
+
 }
 
 output "general-securitygroup" {
